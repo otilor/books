1bbefb4653f6cd249814a44f00d670d45a76c988
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-11-06 22:19:11 +0100

    update code

--- src/main/java/com/humaneguy/Main.java
+++ src/main/java/com/humaneguy/Main.java
@@ -1,7 +1,28 @@
 package com.humaneguy;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/api/greeting", (exchange -> {
+            if ("GET".equals(exchange.getRequestMethod())) {
+                String responseText = "Hello world! from our framework-less REST API\n";
+                exchange.sendResponseHeaders(200, responseText.getBytes().length);
+                OutputStream output = exchange.getResponseBody();
+                output.write(responseText.getBytes());
+                output.flush();
+            } else {
+                exchange.sendResponseHeaders(405, -1);
+            }
+            exchange.close();
+        }));
+
+        server.setExecutor(null);
+        server.start();
     }
 }
