22ccb7d18a38c6f5046b258a0e0f63944c876919
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-11-08 10:10:39 +0100

    update pom

--- src/main/java/com/humaneguy/Main.java
+++ src/main/java/com/humaneguy/Main.java
@@ -1,28 +1,10 @@
 package com.humaneguy;
 
-import com.sun.net.httpserver.HttpServer;
+import static spark.Spark.*;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        server.createContext("/api/greeting", (exchange -> {
-            if ("GET".equals(exchange.getRequestMethod())) {
-                String responseText = "Hello world! from our framework-less REST API\n";
-                exchange.sendResponseHeaders(200, responseText.getBytes().length);
-                OutputStream output = exchange.getResponseBody();
-                output.write(responseText.getBytes());
-                output.flush();
-            } else {
-                exchange.sendResponseHeaders(405, -1);
-            }
-            exchange.close();
-        }));
-
-        server.setExecutor(null);
-        server.start();
+    public static void main(String[] args) {
+        get("/greeting", (req, res) -> "Hello world! from my Spark REST API");
     }
 }
